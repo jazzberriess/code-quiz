f5761550e9014463eefc7a0e75054fd239c8034e
Jazzberries <anime.flick.chick@gmail.com>
2022-03-12 17:20:08 +1100

    udated js file, adding questions

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ A small code quiz designed to test our knowledge of JavaScript.
 The following websites were invaluable in my quest for knowledge and troubleshooting:
 
 [Stack Overflow: How to place and center text in an svg rectangle](https://stackoverflow.com/questions/5546346/how-to-place-and-center-text-in-an-svg-rectangle)
+[Stack Overflow: How to add onclick event to start timer](https://stackoverflow.com/questions/54637148/how-to-add-onclick-event-to-start-timer)
 
 Mozilla Developer Network
 W3 Schools
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,137 @@
-//get ID's
+//creating variables
 
-var questions = document.getQuerySelector(".questions");
-var startButton = document.getQuerySelector("#start-quiz");
-var timerCount = document.getQuerySelector(".timer-count");
+let questions = document.querySelector(".questions");
+let startButton = document.querySelector("#start-quiz");
+let timerCount = document.querySelector(".timer-count");
+let seconds = document.querySelector("#seconds");
+
+let answerSection = document.querySelector(".answer-section");
 
 
-//variables
+let score = 0;
+var index = 0
+
+
+let theQuestions = [
+    {
+        question: "This is q 1",
+        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correctAnswer: "Answer2",
+    },
+    {
+        question: "This is q 2",
+        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correctAnswer: "Answer 4",
+    },
+    {
+        question: "This is q 3",
+        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correctAnswer: "Answer 1",
+    },
+    {
+        question: "This is q 4",
+        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correctAnswer: "Answer 3",
+    },
+    {
+        question: "This is q 5",
+        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        correctAnswer: "Answer 3",
+    },
+];
+
+//create a new h1 for the questions
+
+// let quizQuestion = document.createElement("h1");
+
+//text for the question
+
+// quizQuestion.textContent = "Here's a quiz question!"
+
+//create ordered list for quiz answers
+
+// let answerList = document.createElement("ol");
+// let answerLi1 = document.createElement("li");
+// let answerLi2 = document.createElement("li");
+// let answerLi3 = document.createElement("li");
+// let answerLi4 = document.createElement("li");
+
+//text for the quiz answers
+// answerLi1.textContent = "Answer 1";
+// answerLi2.textContent = "Answer 1";
+// answerLi3.textContent = "Answer 1";
+// answerLi4.textContent = "Answer 1";
+
+
+
+//start button begins timer
+
+startButton.addEventListener("click", function () {
+
+    // function countdown() {
+
+    let timeLeft = 60;
+    //count how many seconds remain
+    let timeInterval = setInterval(function () {
+
+        //if time left is greater than 1, count down and display tramining time.
+
+        if (timeLeft > 1) {
+            timerCount.textContent = timeLeft--;
+
+            //if time left is 1 second, change the word 'seconds' to 'second'
+        } else if (timeLeft === 1) {
+            timerCount.textContent = timeLeft--;
+            seconds.textContent = "second";
+
+            //if time left is zero, clear the counter
+        } else {
+            timerCount.textContent = "0";
+            seconds.textContent = "seconds";
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+});
+
+startButton.addEventListener("click", function () {
+
+    //clear the main text to make room for questions
+    document.querySelector(".questions").innerHTML = "";
+    console.log("clearing the text");
+
+    //variables for quiz questions and answers
+
+    var quizQuestion = theQuestions[index].question;
+    var quizAnswers = theQuestions[index].answers;
+
+    //show quiz question in the text area
+
+    document.querySelector(".questions").append(quizQuestion);
+
+    console.log("hello bees");
+
+    //show the quiz answers
+
+    for (var i = 0; i < quizAnswers.length; i++)
+        console.log(theQuestions[i]);
+
+    var element = document.createElement("button");
+    // element.innerHTML = quizAnswers;
+    element.textContent = quizAnswers;
+
+    questions.appendChild(element);
+
+    console.log(quizAnswers);
+
+
+
+});
 
 
 //start buttons begins questions and countdown timer
 
+
+
 //if answer corret, state 'correct!' and move to next question
 
-//if answer incorrect, state 'wrong', deduct 10 seconds from timer and move to next question
-
+// if answer incorrect, state 'wrong', deduct 10 seconds from timer and move to next question
